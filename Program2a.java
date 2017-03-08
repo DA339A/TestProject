d8d2599cfe76036add223098b8aa39ce3cf613db
DA339A <alexgiang@hotmail.com>
2017-03-08 14:12:31 +0100

    uyiluyil
    ö,luåilpåu

---
+++ Program2a.java
@@ -0,0 +1,33 @@
+package laboration2;
+
+public class Program2a {
+    public void info() {
+        // Lägg till instruktioner här
+    }
+
+    public static void main( String[] args ) {
+    	System.out.println("Hej, mitt namn är Eskil.");
+    	System.out.println("Jag har skrivit 3 program i Java.");
+    	System.out.println("Det är KUL med Java!!!");
+        Program2a p2 = new Program2a();
+        p2.info();
+    }
+}
+
+
+
+
+
+
+//System.out.println();
+//e5a.exercise5a1();
+//System.out.println();
+//e5a.exercise5a2();
+//System.out.println();
+//e5a.exercise5a3();
+//System.out.println();
+//e5a.exercise5a4();
+//System.out.println();
+//e5a.exercise5a5();
+//System.out.println();
+//e5a.exercise5a6();
